d11d68ff5371ee9b4af2d3863c3a66a3ec71f331
Insu-Yoon <110891599+Insu-Yoon@users.noreply.github.com>
2022-09-09 20:59:51 +0900

    [Bronze V] Title: 구구단, Time: 240 ms, Memory: 18516 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int i = s.nextInt();
+        for(int j=1;j<10;j++){
+            System.out.println(i+" * "+j+" = "+i*j);
+        }
+    }
+}
