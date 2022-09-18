a7e85e4ddafcb227de0e50c2eb91ae2d44913ea8
Insu-Yoon <110891599+Insu-Yoon@users.noreply.github.com>
2022-09-18 13:58:45 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char chr = input.next().charAt(0);
+        int out = chr;
+        System.out.println(out);
+    }
+}
