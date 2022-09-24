5de3b583d143459894dc6da13a9b3833f76f63c7
Insu-Yoon <110891599+Insu-Yoon@users.noreply.github.com>
2022-09-24 09:34:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = -1;
+        int cnt = 0;
+        while (n>=0){
+            if(n%5==0) {
+                result = cnt + n/5;
+                break;
+            }
+            n -= 3;
+            cnt++;
+        }
+        System.out.println(result);
+    }
+}
