2ffe414d20c251d7db85cb2be5a6306b11629545
Insu-Yoon <110891599+Insu-Yoon@users.noreply.github.com>
2022-10-01 19:37:50 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        System.out.println(factorial(num));
+    }
+    public static int factorial(int num){  
+        if(num==0) return 1;
+        return num * factorial(num-1);
+    }
+}
