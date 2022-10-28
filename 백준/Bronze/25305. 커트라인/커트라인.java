d52869a57a03a9c86ded7b3afe9e7dd7a1bd1c2d
Insu-Yoon <ingsu814@gmail.com>
2022-10-28 21:45:47 +0900

    [Bronze II] Title: 커트라인, Time: 136 ms, Memory: 14464 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        br.close();
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int cutLine = arr[arr.length-k];
+        bw.write(cutLine+"");
+        bw.close();
+    }
+}
