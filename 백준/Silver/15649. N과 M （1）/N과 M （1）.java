ce78dbbe4c57361bd21e17844b9b9188c4ecf538
Insu-Yoon <ingsu814@gmail.com>
2022-12-01 23:36:55 +0900

    [Silver III] Title: N과 M (1), Time: 1380 ms, Memory: 313500 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,40 +1,43 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-	public static int[] arr;
-	public static boolean[] visit;
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		int M = in.nextInt();
- 
-		arr = new int[M];
-		visit = new boolean[N];
-		dfs(N, M, 0);
- 
-	}
- 
-	public static void dfs(int N, int M, int depth) {
-		if (depth == M) {
-			for (int val : arr) {
-				System.out.print(val + " ");
-			}
-			System.out.println();
-			return;
-		}
- 
-		for (int i = 0; i < N; i++) {
-			if (!visit[i]) {
-				visit[i] = true;
-				arr[depth] = i + 1;
-				dfs(N, M, depth + 1);
-				visit[i] = false;
-			}
-		}
-	}
- 
+
+    public static int[] arr;
+    public static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(N, M, 0);
+
+    }
+
+    public static void dfs(int N, int M, int depth) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        if (depth == M) {
+            for (int val : arr) {
+                bw.write(val + " ");
+            }
+            bw.flush();
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
 }
